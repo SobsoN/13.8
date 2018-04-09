faeb8f272fc12a453e8228acf3c828fcde1fa0bd
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-04-09 16:23:31 +0200

    Fin

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ var fs = require('fs');
 var server = http.createServer();
 server.on('request', function (request, response){
 	response.setHeader("Content-Type", "text/html; charset=utf-8");
-	if (request.method === 'GET' && request.url === 'index.html'){
+	if (request.method === 'GET' && request.url === '/index.html'){
 		fs.readFile('./index.html', function(err, data){
 			if (err) throw err;
 			response.write(data);
